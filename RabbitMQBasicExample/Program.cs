dfebcc556e3fde2d3a20791657b53a706559e6e8
devel <devel@BURAKONEN>
2023-03-21 17:26:38 +0300

    Add project files.

---
+++ RabbitMQBasicExample/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory= new ConnectionFactory();
+factory.Uri = new Uri("amqps://ajussjfj:BBaMFe0vG7foucTCjXK7ABkJUlME2JGj@sparrow.rmq.cloudamqp.com/ajussjfj");
+//CloudAMQP sunucusu aracılığı ile oluşturulan url adresi
+
+using (IConnection connection =  factory.CreateConnection())
+using(IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare("mesajKuyrugu", false, false, true);
+    byte[] byteMessage = Encoding.UTF8.GetBytes("rabbitMq deneme mesajı");
+    channel.BasicPublish(exchange: "", routingKey: "mesajKuyrugu", body: byteMessage);
+
+}
+
+//ConnenctionFactor sınıfı ile bağlantı adresini tanımlıyoruz.
+//CreateConnection ile bağlantıyı sağlıyoruz.
+//CreateModel ile kullanacağımız kuyruğu oluşturabilmek için kanal kuruyoruz.,
---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://ajussjfj:BBaMFe0vG7foucTCjXK7ABkJUlME2JGj@sparrow.rmq.cloudamqp.com/ajussjfj");
+
+
+using (IConnection connection = factory.CreateConnection())
+using (IModel channel = connection.CreateModel())
+{
+    channel.QueueDeclare("mesajKuyrugu", false, false, true);
+    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+    channel.BasicConsume("mesajKuyrugu", false, consumer);
+    consumer.Received += (sender, e) =>
+    {
+        //e.Body : Kuyruktaki mesajı verir.
+        Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    };
+}
+Console.Read();
